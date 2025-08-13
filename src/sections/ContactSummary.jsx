976255a5bcfc9b37dcd8443645812b567f45b005
MohitCode17 <mohit.codes17@gmail.com>
2025-08-13 22:59:47 +0530

    add conract summary section with marquee animation

---
+++ src/sections/ContactSummary.jsx
@@ -0,0 +1,45 @@
+import Marquee from "../components/Marquee";
+
+const ContactSummary = () => {
+  const items = [
+    "Curiosity",
+    "Craftsmanship",
+    "Growth",
+    "Resilience",
+    "Vision",
+  ];
+
+  const items2 = [
+    "contact us",
+    "contact us",
+    "contact us",
+    "contact us",
+    "contact us",
+  ];
+
+  return (
+    <section className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16">
+      {/* MARQUEE ANIMATION */}
+      <Marquee items={items} />
+
+      <div className="font-light text-center contact-text-responsive overflow-hidden">
+        <p>
+          “ Let’s build a <br />
+          <span className="font-normal">memorable</span> &{" "}
+          <span className="italic">meaningful</span> <br />
+          web app <span className="text-gold">together</span> “
+        </p>
+      </div>
+
+      <Marquee
+        items={items2}
+        reverse={true}
+        className="text-black bg-transparent border-y-2"
+        iconClassName="stroke-gold stroke-2 text-primary"
+        icon="material-symbols-light:square"
+      />
+    </section>
+  );
+};
+
+export default ContactSummary;
