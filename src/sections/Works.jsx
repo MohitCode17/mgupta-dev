a425336ee1aa17503a59cacb6d8cbf5d46201355
MohitCode17 <mohit.codes17@gmail.com>
2025-08-14 23:50:36 +0530

    add project link for project

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -110,58 +110,63 @@ and lasting impact.`;
         onMouseMove={handleMouseMove}
       >
         {projects.map((project, index) => (
-          <div
-            key={project.id}
-            id="project"
-            className="relative flex flex-col gap-1 py-5 cursor-pointer group md:gap-0"
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave(index)}
-          >
-            {/* OVERLAY */}
+          <a href={project.href} target="_blank" key={project.id}>
             <div
-              ref={(el) => {
-                overlayRefs.current[index] = el;
-              }}
-              className="absolute inset-0 hidden md:block duration-200 bg-black -z-10 clip-path"
-            />
-
-            {/* TITLE */}
-            <div className="flex justify-between px-5 sm:px-10 text-black transition-all duration-500 md:group-hover:px-12 md:group-hover:text-white">
-              <h2 className="lg:text-[32px] text-[26px] leading-none">
-                {project.name}
-              </h2>
-              <Icon icon="lucide:arrow-up-right" className="md:size-6 size-5" />
-            </div>
-
-            {/* DIVIDER */}
-            <div className="w-full h-0.5 bg-black/80" />
-
-            {/* TECHNOLOGIES */}
-            <div className="flex flex-wrap px-5 sm:px-10 text-xs leading-loose uppercase transtion-all duration-500 md:text-sm gap-x-5 md:group-hover:px-12">
-              {project.frameworks.map((framework) => (
-                <p
-                  key={framework.id}
-                  className="text-black transition-colors duration-500 md:group-hover:text-white"
-                >
-                  {framework.name}
-                </p>
-              ))}
-            </div>
-
-            {/* MOBILE PREVIEW IMAGE */}
-            <div className="relative flex items-center justify-center px-5 sm:px-10 md:hidden h-[400px]">
-              <img
-                src={project.bgImage}
-                alt={`${project.name}-bg-image`}
-                className="object-cover w-full h-full rounded-md brightness-50"
+              key={project.id}
+              id="project"
+              className="relative flex flex-col gap-1 py-5 cursor-pointer group md:gap-0"
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+            >
+              {/* OVERLAY */}
+              <div
+                ref={(el) => {
+                  overlayRefs.current[index] = el;
+                }}
+                className="absolute inset-0 hidden md:block duration-200 bg-black -z-10 clip-path"
               />
-              <img
-                src={project.image}
-                alt={`${project.name}-image`}
-                className="absolute px-14 rounded-xl bg-center"
-              />
+
+              {/* TITLE */}
+              <div className="flex justify-between px-5 sm:px-10 text-black transition-all duration-500 md:group-hover:px-12 md:group-hover:text-white">
+                <h2 className="lg:text-[32px] text-[26px] leading-none">
+                  {project.name}
+                </h2>
+                <Icon
+                  icon="lucide:arrow-up-right"
+                  className="md:size-6 size-5"
+                />
+              </div>
+
+              {/* DIVIDER */}
+              <div className="w-full h-0.5 bg-black/80" />
+
+              {/* TECHNOLOGIES */}
+              <div className="flex flex-wrap px-5 sm:px-10 text-xs leading-loose uppercase transtion-all duration-500 md:text-sm gap-x-5 md:group-hover:px-12">
+                {project.frameworks.map((framework) => (
+                  <p
+                    key={framework.id}
+                    className="text-black transition-colors duration-500 md:group-hover:text-white"
+                  >
+                    {framework.name}
+                  </p>
+                ))}
+              </div>
+
+              {/* MOBILE PREVIEW IMAGE */}
+              <div className="relative flex items-center justify-center px-5 sm:px-10 md:hidden h-[400px]">
+                <img
+                  src={project.bgImage}
+                  alt={`${project.name}-bg-image`}
+                  className="object-cover w-full h-full rounded-md brightness-50"
+                />
+                <img
+                  src={project.image}
+                  alt={`${project.name}-image`}
+                  className="absolute px-14 rounded-xl bg-center"
+                />
+              </div>
             </div>
-          </div>
+          </a>
         ))}
 
         {/* DESKTOP FLOATING PREVIEW IMAGE */}
